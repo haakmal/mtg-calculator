9322b8ba11563e9dd1ec648d40fa227d8a60483f
Haider Akmal <36928871+haideraliakmal@users.noreply.github.com>
2023-04-14 12:48:42 +1000

    Added First Strike

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # A Magic the Gathering Damage Calculator
 
-Made this mostly to practice some front-end development but it is helpful now and then for more complex MtG rounds.
+To help myself and my MtG group learn the game I made this damage calculator. I wanted something to practice some coding one as I hadn't done any in a long time and was starting to feel I was getting rusty. Will most likely update this with more things as I understand the game better.
+
+[Test it for yourself](https://haideraliakmal.github.io/mtg-calculator) and if you use it in your games give me a [shout](https://twitter.com/haideraliakmal)!
+
+# How to use it
+
+Go to the [live page](https://haideraliakmal.github.io/mtg-calculator) and enter in the stats for your *Attacker* and *Blocker* cards. Select any attributes that apply like *First Strike*, *Deathtouch*, etc, and hit the Battle button!
+
+## What can this calculator do
+
+Also what I'm planning to add ahead for practice.
+
+- [x] Calculate damage
+- [ ] Apply First Strike
+- [ ] Apply Double Strike
+- [ ] Apply Deathtouch
+- [ ] Apply Lifelink
+- [ ] Show some flavour text
+- [ ] Calculate any counters
+
+**PS.** I'm still working on this and my coding skills aren't the best so if anyone wants to fork this and improve it feel free!
--- script.js
+++ script.js
@@ -1,56 +1,74 @@
 function Calculate() {
+  // Start with clean slate, remove message
+  var msg1 = document.querySelector(".msg1")
+  var msg2 = document.querySelector(".msg2")  
+  msg1.classList.remove("lose", "win");
+  msg2.classList.remove("lose", "win");
   
-    var msg1 = document.querySelector(".msg1")
-    var msg2 = document.querySelector(".msg2")
-    
-    msg1.classList.remove("lose", "win");
-    msg2.classList.remove("lose", "win");
-  
+  // Attacker card values
   var attackArr = [
     Number(document.getElementById('p1-strength').value),
     Number(document.getElementById('p1-toughness').value),
     document.getElementById('p1-fstrike').checked,
     document.getElementById('p1-dstrike').checked]
-  
+  // Blocker card values
   var blockArr = [
     Number(document.getElementById('p2-strength').value),
     Number(document.getElementById('p2-toughness').value),
     document.getElementById('p2-fstrike').checked,
-    document.getElementById('p2-dstrike').checked]  
+    document.getElementById('p2-dstrike').checked]    
   
-  var attackRes = attackArr[0] - blockArr[1]
-  var blockRes = attackArr[1] - blockArr[0]
+  var attackRes1
+  var blockRes1
+  var attackRes2
+  var blockRes2
   
-
-  document.getElementById("p1-result").innerHTML = 
-      (attackArr[0] - blockArr[1]) +
-      "/" +
-      (attackArr[1] - blockArr[0]);
+  FirstStrike();
+  Fight();
+  Assign();
+  Check();
   
-  
-  document.getElementById("p2-result").innerHTML = 
-      (blockArr[0] - attackArr[1]) +
-      "/" +
-      (blockArr[1] - attackArr[0]);
-  
-  
-  
-  function Check() {
-    
-    if (attackArr[1] <= 0) {
-      msg1.classList.add("lose");
-    }
-    if (blockArr[1] <= 0) {
-      msg2.classList.add("lose");
-    }
-    if (attackArr[1] > 0) {
-      msg1.classList.add("win");
-    }
-    if (blockArr[1] > 0) {
-      msg2.classList.add("win");
+  // Calculate First Strike
+  function FirstStrike() {
+    if (attackArr[2] === true) {
+      blockArr[1] = blockArr[1] - attackArr[0]
     }
   }
   
-  Check();
+  // Calculate Battle
+  function Fight() {
+    attackRes1 = attackArr[0] - blockArr[1]
+    blockRes1 = attackArr[1] - blockArr[0]
+    attackRes2 = blockArr[0] - attackArr[1]
+    blockRes2 = blockArr[1] - attackArr[0]
+  }
+  
+  // Calculate Double Strike
+  function DoubleStrike() {
+    if (attackArr[3] === true) {
+      attackArr[0] == attackRes1
+      attackRes1 = attackArr[0] - blockArr[1]
+      blockRes1 = attackArr[1] - blockArr[0]
+      attackRes2 = blockArr[0] - attackArr[1]
+      blockRes2 = blockArr[1] - attackArr[0]
+    }
+  }
+  
+  // Assign attack and block to card
+  function Assign() {
+    document.getElementById("p1-result").innerHTML = attackRes1 + "/" + blockRes1;
+    document.getElementById("p2-result").innerHTML = attackRes2 + "/" + blockRes2;
+  }
+  
+  // Check result and update message
+  function Check() {
+    if (blockRes1 > 0) {
+      msg1.classList.add("win");
+    } else {msg1.classList.add("lose");};
+    
+    if (blockRes2 > 0) {
+      msg2.classList.add("win");
+    } else {msg2.classList.add("lose");};
+  }
   
   }
